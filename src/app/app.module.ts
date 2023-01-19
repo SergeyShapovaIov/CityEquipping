21c6f07b0e7ab8b564063dc34ee6aaf479a65c1d
Sergey Shapovalov <Shapovalov.Sergey2016@yandex.ru>
2023-01-19 18:58:55 +0300

    created new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardContainerComponent } from './components/card-container/card-container.component';
+import { GraphComponent } from './components/graph/graph.component';
+import { NgxEchartsModule } from 'ngx-echarts';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardContainerComponent
+    CardContainerComponent,
+    GraphComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts')
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
